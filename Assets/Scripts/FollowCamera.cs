dab92a82295a524934bb3685a40b0a42662936b1
Carl Dunham <cdunham@gmail.com>
2016-07-14 20:11:04 -0400

    Initial working version

---
+++ Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowCamera : MonoBehaviour {
+
+    public Transform transformToFollow;
+    public float smooth = 1;
+
+    private Vector3 offset;
+
+    void Start() {
+        offset = this.transform.position - transformToFollow.position;
+        MoveCamera();
+    }
+    
+    void LateUpdate() {
+        MoveCamera();
+    }
+
+    private void MoveCamera() {
+        Vector3 moveTo = transformToFollow.position + offset;
+        this.transform.position = Vector3.Lerp(this.transform.position, moveTo, Time.deltaTime * smooth);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+    public float speed = 10;
+
+    private Rigidbody rb;
+
+    void Start() {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate() {
+        float moveH = Input.GetAxis("Horizontal");
+        float moveV = Input.GetAxis("Vertical");
+
+        Vector3 vec = new Vector3(moveH, 0.0f, moveV);
+
+        rb.AddForce(vec * speed);
+    }
+}
