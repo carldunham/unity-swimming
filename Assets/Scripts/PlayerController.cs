c76eeb07355beb6dedad3453ec448e382a075739
Carl A Dunham <cdunham@gmail.com>
2016-07-15 10:43:55 -0400

    [FEATURE] Make PlayerController use Animator

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,20 +3,44 @@ using UnityEngine.UI;
 using System.Collections;
 
 public class PlayerController : MonoBehaviour {
-    public float speed = 10;
+    public float speedTurnThreshold = 0.1f;
+    public float directionDeltaSpeed = 2.0f;
 
-    private Rigidbody rb;
+    private Animator anim;
+    private float lastDirection = 0.0f;
+
+    private const float EPSILON = 0.001f;
+
 
     void Start() {
-        rb = GetComponent<Rigidbody>();
+        anim = this.GetComponent<Animator>();
     }
 
     void FixedUpdate() {
-        float moveH = Input.GetAxis("Horizontal");
-        float moveV = Input.GetAxis("Vertical");
+        float targetDirection = Input.GetAxisRaw("Horizontal");
+        float rawSpeed = Input.GetAxisRaw("Vertical");
+        float speed = Input.GetAxis("Vertical");
+        float direction;
 
-        Vector3 vec = new Vector3(moveH, 0.0f, moveV);
+        if (Mathf.Abs(rawSpeed) < this.speedTurnThreshold) {
+            targetDirection = 0.0f;
+        }
 
-        rb.AddForce(vec * speed);
+        if (targetDirection != this.lastDirection) {
+            // direction = Mathf.SmoothStep(this.lastDirection, rawDirection, Time.deltaTime * directionDeltaSpeed);
+
+            float sign = (targetDirection < this.lastDirection) ? -1f : 1f;
+            float delta = Time.deltaTime * directionDeltaSpeed;
+            direction = Mathf.Clamp(this.lastDirection + (sign * delta), -1f, 1f);
+
+            if ((targetDirection == 0f) && (Mathf.Abs(direction) < EPSILON)) direction = 0f;
+        }
+        else {
+            direction = this.lastDirection;
+        }
+        this.lastDirection = direction;
+
+        anim.SetFloat("direction", direction);
+        anim.SetFloat("speed", speed);
     }
 }
